e11142074b6a201d621d8d23e568ebcc7474e75c
git162 <dassudeep50@gmail.com>
2024-10-30 03:26:11 +0530

    some skeleton files added

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,3 +1,8 @@
 const { Router } = require("express");
 const router = Router();
+const userAuth = require("../auth/userAuth");
+const userController = require("../controllers/")
 
+router.post('/signup',userAuth.userSignup,userController.createUser);
+router.post('/signin',userAuth.userSignin);
+router.get('/:userid',userController.getUser);
